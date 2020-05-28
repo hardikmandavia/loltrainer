c6d7899ad7fb182b2cb7efc4365a6170d8e97dcf
Hardik Mandavia <hardik.mandavia@deliveroo.co.uk>
2020-05-28 15:40:41 +0100

    removed redux

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,11 @@
 import 'react-native-gesture-handler';
 import React, { Component } from 'react';
-import { createStore, applyMiddleware } from 'redux';
-import { Provider, connect } from 'react-redux';
-import axios from 'axios';
-import axiosMiddleware from 'redux-axios-middleware';
+import { Platform } from 'react-native';
+
+
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import SplashScreen from 'react-native-splash-screen';
-
-import reducer from './store';
 
 import Home from './components/Home';
 import Summoner from './components/Summoner';
@@ -22,12 +19,6 @@ declare const global: { HermesInternal: null | {} };
 const Stack = createStackNavigator<RootStackParamList>();
 const { Navigator, Screen } = Stack;
 
-const client = axios.create({
-  baseURL: '',
-  responseType: 'json'
-});
-
-const store = createStore(reducer, applyMiddleware(axiosMiddleware(client)));
 
 export default class App extends Component {
   componentDidMount() {
@@ -36,16 +27,14 @@ export default class App extends Component {
 
   render() {
     return (
-      <Provider store={store}>
-        <NavigationContainer>
-          <Container>
-            <Navigator headerMode="none">
-              <Screen name={Routes.HOME} component={Home} />
-              <Screen name={Routes.SUMMONER} component={Summoner} />
-            </Navigator>
-          </Container>
-        </NavigationContainer>
-      </Provider>
+      <NavigationContainer>
+        <Container>
+          <Navigator headerMode="none">
+            <Screen name={Routes.HOME} component={Home} />
+            <Screen name={Routes.SUMMONER} component={Summoner} />
+          </Navigator>
+        </Container>
+      </NavigationContainer>
     );
   }
 };
